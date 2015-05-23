e678b992e1b29b8a556553cd42e57924640659dc
Kyle McDonald <kyle@kylemcdonald.net>
2015-05-23 02:32:37 -0400

    saving images to disk regularly for visualizing later

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var mkdirp = require('mkdirp');
+var bodyParser = require('body-parser')
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -5,6 +8,7 @@ var io = require('socket.io')(server);
 
 server.listen(8000);
 
+app.use(bodyParser.raw({ type: 'image/jpeg', limit: '10mb'}));
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
@@ -13,8 +17,25 @@ io.on('connection', function (socket) {
 		console.log(data);
 	});
 
-	app.get('/voicemail', function(res, req) {
-		io.sockets.emit('voicemail', res.query);
-		req.sendStatus(200);
+	app.get('/voicemail', function(req, res) {
+		io.sockets.emit('voicemail', req.query);
+		res.sendStatus(200);
 	})
 });
+
+app.post('/screenshot', function(req, res) {
+	var cameraId = req.query.cameraId;
+	var dir = 'data/screenshots/' + cameraId + '/';
+	mkdirp(dir, function (err) {
+	    if (err) {
+	    	console.error(err);
+	    	return;
+	    }
+		var filename = dir + (new Date()) + '.jpg';
+		console.log('saving image to ' + filename);
+		fs.writeFile(filename, req.body, 'binary', function(err) {
+			if(err) console.log(err);
+		});
+	});
+	res.sendStatus(200); // respond immediately
+})
