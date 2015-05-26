c5269d5ea93a190b946427ba1034173849e47390
Kyle McDonald <kyle@kylemcdonald.net>
2015-05-26 03:54:45 -0400

    fixed phantom bug with axes on initialization. added all libs to repo

--- public/lib/axes.js
+++ public/lib/axes.js
@@ -7,28 +7,29 @@ var xScale = d3.scale.linear().range([0, axesWidth]),
       .ticks(10)
       .tickSubdivide(true);
 
-var svg = d3.select('#axes')
+var axes = d3.select('#axes')
     .attr('width', axesWidth)
     .attr('height', axesHeight);
 
-svg.append('g')
+axes.append('g')
     .attr('class', 'x axis')
     .attr('transform', 'translate(0,' + axesHeight + ')')
 
 var axesInitialized = false;
 function zoomToRange(left, right, padding, duration) {
-  if(!axesInitialized) {
-    duration = 0;
-    axesInitialized = true;
-  }
   var range = right - left;
   left -= range * padding;
   right += range * padding;
+  var base = axes;
   xScale.domain([ left, right ])
-    svg.transition()
-      .duration(duration)
-      .select('.x.axis')
-      .call(xAxis)
-      .selectAll('text')
-        .attr('transform', 'translate(5,-28)')
+  if(axesInitialized) {
+    base = base.transition()
+      .duration(duration);
+  } else {
+    axesInitialized = true;
+  }
+  base.select('.x.axis')
+    .call(xAxis)
+    .selectAll('text')
+      .attr('transform', 'translate(5,-28)');
 }
